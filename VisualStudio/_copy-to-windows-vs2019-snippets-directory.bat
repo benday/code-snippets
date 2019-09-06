160dfedcd0c4736b1d53b0cddd6c3ab1849f1c4e
Benjamin Day <benday@benday.com>
2019-09-06 15:04:28 -0400

    Added Visual Studio snippets

---
+++ VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
@@ -0,0 +1,1 @@
+copy /y *.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
---
+++ VisualStudio/_open-windows-vs2019-snippets-directory.bat
@@ -0,0 +1,1 @@
+explorer %userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets
