7f4c90aea7c988579f2749e3690a54bfc188fd54
Benjamin Day <benday@benday.com>
2020-04-02 13:56:04 -0400

    Added snippets for aeb, aneb, and nls.

--- VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
+++ VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
@@ -1,1 +1,5 @@
+IF NOT EXIST "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets" (
+    mkdir "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
+)
+
 copy /y *.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
--- VisualStudioCode/_copy-to-windows-snippets-directory.bat
+++ VisualStudioCode/_copy-to-windows-snippets-directory.bat
@@ -1,1 +1,6 @@
+IF NOT EXIST "%appdata%\Code\User\snippets\" (
+    mkdir "%appdata%\Code\User\snippets\"
+)
+
+
 copy /y *.json "%appdata%\Code\User\snippets\"
