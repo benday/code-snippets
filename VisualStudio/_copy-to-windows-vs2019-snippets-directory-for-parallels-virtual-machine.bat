0dc5adcde57f7696478baee821fee65bbc6b02cc
Benjamin Day <benday@benday.com>
2020-04-27 11:02:26 -0400

    Added some snippets for asserting string empty

---
+++ VisualStudio/_copy-to-windows-vs2019-snippets-directory-for-parallels-virtual-machine.bat
@@ -0,0 +1,5 @@
+IF NOT EXIST "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets" (
+    mkdir "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
+)
+
+copy /y *.snippet "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
