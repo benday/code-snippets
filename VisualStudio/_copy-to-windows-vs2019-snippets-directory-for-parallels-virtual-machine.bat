2058ce85d81710e677559e237aff183da8b22a10
Benjamin Day <benday@benday.com>
2020-11-13 10:54:26 -0500

    added some xaml snippets for visual studio. reorged visual studio folder structure

--- VisualStudio/_copy-to-windows-vs2019-snippets-directory-for-parallels-virtual-machine.bat
+++ VisualStudio/_copy-to-windows-vs2019-snippets-directory-for-parallels-virtual-machine.bat
@@ -3,3 +3,11 @@ IF NOT EXIST "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\Visual C#\My
 )
 
 copy /y *.snippet "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
+
+IF NOT EXIST "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets" (
+    mkdir "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets"
+)
+
+copy /y *.snippet "\\Mac\Home\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets"
+
+
--- VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
+++ VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
@@ -2,4 +2,9 @@ IF NOT EXIST "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#
     mkdir "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
 )
 
-copy /y *.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
+IF NOT EXIST "%userprofile%\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets" (
+    mkdir "%userprofile%\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets"
+)
+
+copy /y .\csharp\*.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
+copy /y .\xaml\*.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\XAML\My XAML Snippets"
